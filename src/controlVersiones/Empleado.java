68c5bc2c3056afd3b0a3eaaa94d5cf2dc9902e44
Alex <lexurt@gmail.com>
2021-05-24 09:48:47 +0200

    Añadido el Javadoc a la clase Empleado

--- src/controlVersiones/Empleado.java
+++ src/controlVersiones/Empleado.java
@@ -1,10 +1,27 @@
 package controlVersiones;
-
+/**
+ * 
+ * @author Alex Hurtado
+ * @version Version 1, 2021
+ * @since 24/05/2021
+ *
+ */
 public class Empleado {
+	/**
+	 * <i>private String nombre</i>: es el nombre del empleado
+	 * <i>private String apellidos</i>: son los apellidos del empleado
+	 * <i>private double salario</i>: es el salario del empleado
+	 */
 	private String nombre;
 	private String apellidos;
 	private double salario;
-
+	
+	/**
+	 * Constructor de la clase Empleado que recibe todos los atributos
+	 * @param nombre nombre del empleado
+	 * @param apellido apellidos del Empleado
+	 * @param salario salario del empleado
+	 */
 	public Empleado(String nombre, String apellido, double salario) {
 		this.nombre = nombre;
 		this.apellidos = apellido;
@@ -34,7 +51,10 @@ public class Empleado {
 	public void setSalario(double salario) {
 		this.salario = salario;
 	}
-
+	/**
+	 * Método para subir el salario del empleado
+	 * @param subida cantidad que se sube el salario del Empleado
+	 */
 	public void subirsalario(double subida) {
 		salario = salario + subida;
 	}
